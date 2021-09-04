754a58dbc7b87a5704de3646e3dfd39645b58aec
Mary Smith <mary.sunshine4@gmail.com>
2021-09-04 00:56:25 +0000

    Further study done

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,10 +2,49 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+  const result = new Set();
+
+  for (const word of words1Set) {
+    if (words2Set.has(word)) {
+      result.add(word)
+    }
+  }
+  
+  return (Array.from(result));
 }
+console.log(wordsInCommon(['I', 'me', 'me', 'she', 'it'], ['I', 'he', 'it']))
 
 
 function kidsGame(names) {
-  // Replace this with your code
+  const output = [names.shift()];
+ 
+  const firstLettersToWords = {};
+
+  for (const name of names) {
+    if (firstLettersToWords[name[0]] === undefined){
+      firstLettersToWords[name[0]] = [name];
+    }
+    else {
+      firstLettersToWords[name[0]].push(name);
+    }
+  }
+
+  while (true) {
+    const lastWord = output[output.length - 1];
+    const startLetter = lastWord[lastWord.length - 1];
+
+    if (
+      firstLettersToWords[startLetter] === undefined ||
+      firstLettersToWords[startLetter].length === 0
+      ) {
+      break;
+    }
+    let word = firstLettersToWords[startLetter].shift(0);
+    output.push(word);
+  }
+  
+  return output;  
 }
+console.log(kidsGame(["noon", "naan", "nun"]))
